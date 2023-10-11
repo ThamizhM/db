1f242d81bf87446610b1ba31b02eacb1183eff44
thamizhM <thamizhthamizh207@gmail.com>
2023-10-11 14:07:12 +0530

    modified

---
+++ db2.py
@@ -0,0 +1,33 @@
+import pymysql
+host = 'ENMA'
+user = 'root'
+password = ''
+database = 'student'
+connection = pymysql.connect(host=host, user=user, password=password, database=database)
+
+try:
+    with connection.cursor() as cursor:
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS example_table (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            age INT
+        )
+        """
+        cursor.execute(create_table_query)
+        insert_records_query = """
+        INSERT INTO example_table (name, age) VALUES
+        ('John Doe', 25),
+        ('Jane Smith', 30),
+        ('Bob Johnson', 22)
+        """
+        cursor.execute(insert_records_query)
+    connection.commit()
+
+    print("Table created and records inserted successfully!")
+
+except pymysql.Error as e:
+    print(f"Error: {e}")
+
+finally:
+    connection.close()
